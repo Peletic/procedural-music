553fba25e25db91e45b2cb1d54bd0c1a0ad87d8b
peletic <its.peletic@gmail.com>
2025-12-18 13:27:19 -0800

    finish beat framework

--- src/units/beat.ts
+++ src/units/beat.ts
@@ -3,9 +3,15 @@
 import {JoinedNumberCombinations, NumRange} from "../helpers/types";
 
 export class Beat {
-    constructor(length : BeatLength , level : BeatLevel) {
-        const duration = length === 1 ? 1 : 3
-        const denominator = length === 1 ? Math.pow(2, level-1) : Math.pow(2, level)
+    public denominator : number
+    public dotted : boolean
+    public name : string
+
+    constructor(duration : NoteDuration) {
+        let drSplit = duration.toString().split("/")
+        this.denominator = parseInt(drSplit[1])
+        this.dotted = duration[0].endsWith(".5")
+        this.name = BEAT_NAMES[duration]
     }
 }
 
@@ -13,7 +19,13 @@ export type BeatLevel = NumRange<1, 6>
 export type BeatLength = 1 | 1.5
 export type NoteDuration = JoinedNumberCombinations<"/", BeatLength, BeatLevel>
 
-const test : {[p in NoteDuration]: number}= {
-
+export const FULL_BEAT_NAMES : {[p in JoinedNumberCombinations<"/", 1, BeatLevel>]: string}= {
+    "1/1": "whole_note",
+    "1/2": "half_note",
+    "1/3": "quarter_note",
+    "1/4": "eighth_note",
+    "1/5": "sixteenth_note",
+    "1/6": "thirty_second_note"
 }
-console.log(test)
+export const DOTTED_BEAT_NAMES : {[p in JoinedNumberCombinations<"/", 1.5, BeatLevel>]: string} = Object.fromEntries(Object.entries(FULL_BEAT_NAMES).map(([key, val]) => [key.replace("1/", "1.5/"), `dotted_${val}`])) as unknown as {[p in JoinedNumberCombinations<"/", 1.5, BeatLevel>]: string}
+export const BEAT_NAMES : {[p in NoteDuration]: string} = {...FULL_BEAT_NAMES, ...DOTTED_BEAT_NAMES}
