ddb05dcfc2680d7a608c981e5d4d9a9ec753baf3
Peletic <its.peletic@gmail.com>
2025-12-30 02:13:42 -0800

    implement scales

--- src/units/key.ts
+++ src/units/key.ts
@@ -1,9 +1,49 @@
-import {Pitch} from "./pitch";
-import {ToneOctave} from "./tone";
+import {NUMBER_TONE_LOOKUP, Tone, TONE_NUMBER_LOOKUP} from "@/src/units/tone";
 
-for (let i = 0; i <= 112; i++) {
-    const pitch = new Pitch(i);
-    const pitchA = Pitch.of(pitch.value)
-    const pitchB = Pitch.of((pitchA.tone + pitchA.octave) as ToneOctave)
-    console.assert()
+export abstract class Scale {
+    public root: Tone
+    public rootValue: number
+
+    public notes: Tone[7] = []
+    public noteValues: number[7] = []
+
+    abstract get intervals(): number[7]
+
+    protected constructor(root: Tone | number) {
+        if (typeof root === 'number') {
+            this.root = NUMBER_TONE_LOOKUP[root]
+            this.rootValue = root
+        } else {
+            this.root = root
+            this.rootValue = TONE_NUMBER_LOOKUP[root]
+        }
+
+        let sum = 0
+        for (const interval of this.intervals) {
+            sum += interval
+            this.notes.push(NUMBER_TONE_LOOKUP[(this.rootValue + sum) % 12])
+            this.noteValues.push(this.rootValue + sum)
+        }
+    }
+}
+
+export class MajorScale extends Scale {
+    get intervals() {
+        return [0, 2, 2, 1, 2, 2, 2, 1]
+    }
+
+    constructor(root: Tone | number) {
+        super(root);
+    }
+
+}
+
+export class MinorScale extends Scale {
+    get intervals() {
+        return [0, 2, 1, 2, 2, 1, 2, 2]
+    }
+
+    constructor(root: Tone | number) {
+        super(root);
+    }
 }
