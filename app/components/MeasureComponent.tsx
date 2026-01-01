aea6b2916e62468b6fb56fe16bae3dd0d554054f
Peletic <its.peletic@gmail.com>
2026-01-01 05:19:53 -0800

    attempt no 4

--- app/components/MeasureComponent.tsx
+++ app/components/MeasureComponent.tsx
@@ -8,15 +8,15 @@ export default function MeasureComponent({value, idxN, last}: { value: Measure, 
     return (<div className={"bg-white h-[7.3em] border-black flex flex-col-reverse justify-between"}
                  style={{width: `${width}em`, borderRight: `${(idxN + 1) % 3 == 0 ? "2px" : "0px"} solid black`}}>
         <div className={"h-[3em]"}>
-            <img className={"absolute h-[3em] border-r-[2px] border-black"} style={{width: `${12.5}em`}}
+            <img className={"absolute h-[3em] border-r-[2px] object-fill border-black"} style={{width: `${12.5}em`}}
                  src={"staff.svg"}/>
-            {last && <img className={"absolute h-[3em] border-r-[2px] border-black ml-[6.7em]"} style={{width: `${6}em`}}
+            {last && <img className={"absolute h-[3em] border-r-[2px] object-fill border-black ml-[6.7em]"} style={{width: `${6}em`}}
                          src={"staff-end.svg"}/>}
         </div>
         <div className={"h-[3em]"}>
-            <img className={"absolute h-[3em] border-r-[2px] border-black"} style={{width: `${12.5}em`}}
+            <img className={"absolute h-[3em] border-r-[2px] border-black object-fill"} style={{width: `${12.5}em`}}
                  src={"staff.svg"}/>
-            {last && <img className={"absolute h-[3em] border-r-[2px] border-black ml-[6.7em]"} style={{width: `${6}em`}}
+            {last && <img className={"absolute h-[3em] border-r-[2px] border-black object-fill ml-[6.7em]"} style={{width: `${6}em`}}
                          src={"staff-end.svg"}/>}
             {Object.entries(value.collection).map(([pos, notes]) => notes.map((el) => <NoteComponent note={el as Note}
                                                                                                      pos={pos as ElementPosition}
