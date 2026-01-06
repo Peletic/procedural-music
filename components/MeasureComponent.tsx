6a4141032998a563378f7c614db0863048a2135b
Peletic <its.peletic@gmail.com>
2026-01-05 16:44:25 -0800

    lines

--- components/MeasureComponent.tsx
+++ components/MeasureComponent.tsx
@@ -9,15 +9,15 @@ export default function MeasureComponent({value, idxN, last}: { value: Measure, 
                  style={{width: `${width}em`, borderRight: `${(idxN + 1) % 3 == 0 ? "2px" : "0px"} solid black`}}>
         <div className={"h-[3em]"}>
             <img className={"absolute h-[3em] border-r-2 object-fill border-black"} style={{width: `${12.5}em`}}
-                 src={"staff.svg"}/>
+                 src={"staff.svg"} height={"48px"}/>
             {last && <img className={"absolute h-[3em] border-r-2 object-fill border-black ml-[6.7em]"} style={{width: `${6}em`}}
-                         src={"staff-end.svg"}/>}
+                         src={"staff-end.svg"} height={"48px"}/>}
         </div>
         <div className={"h-[3em]"}>
             <img className={"absolute h-[3em] border-r-2 border-black object-fill"} style={{width: `${12.5}em`}}
-                 src={"staff.svg"}/>
+                 src={"staff.svg"} height={"48px"}/>
             {last && <img className={"absolute h-[3em] border-r-2 border-black object-fill ml-[6.7em]"} style={{width: `${6}em`}}
-                         src={"staff-end.svg"}/>}
+                         src={"staff-end.svg"} height={"48px"}/>}
             {Object.entries(value.collection).map(([pos, notes]) => [...new Set(notes)].map((el) => <NoteComponent note={el as Note}
                                                                                                      pos={pos as ElementPosition}
                                                                                                      key={`${JSON.stringify(el)}::${pos}::${(el as Note).duration}::${idxN}::${Math.random()*10}`}/>))}
