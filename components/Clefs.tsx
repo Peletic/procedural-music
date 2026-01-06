16d18177bbeea86d0932eccb99bf3f995a7d5c91
Peletic <its.peletic@gmail.com>
2026-01-05 16:56:22 -0800

    okay safari

--- components/Clefs.tsx
+++ components/Clefs.tsx
@@ -1,6 +1,6 @@
 export default function Clefs({}) {
     return (<div className={"h-[7.3em] flex flex-col justify-between border-l-2 border-black"}>
-    <img className={"absolute h-[7.3em] left-4"} src={"/staff-start.svg"} height={"116.8px"}/>
+    <img className={"absolute h-[7.3em] left-3.5"} src={"/staff-start.svg"} height={"116.8px"} width={"30px"}/>
         <div className={"h-[3em] w-[3.1em]"}>
             <img className={"absolute border-black object-fill"} style={{width: `${3.1}em`, height: `${3}em`}}
                  src={"staff.svg"} width={"49.6px"} height={"48px"}/>
